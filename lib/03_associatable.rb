eda1f2f08edaa49fca314d1c454071c102a8b4c1
eliraybon <elr0615@gmail.com>
2020-01-26 15:47:45 -0500

    Update belongs_to and has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -1,7 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor :foreign_key, :class_name, :primary_key
 
@@ -43,7 +42,7 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
+
   def belongs_to(name, options = {})
     self.assoc_options[name] = BelongsToOptions.new(name, options)
 
